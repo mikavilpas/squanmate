3166ef68613e79e3e42b9194db988822d2a449fc
Mika Vilpas <mika.vilpas@gmail.com>
2017-05-27 21:25:25 +0300

    initial commit

---
+++ project.clj
@@ -0,0 +1,70 @@
+(defproject squanmate "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :min-lein-version "2.7.1"
+  
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.229"]
+                 [devcards "0.2.3"]
+                 [sablono "0.7.4"]
+                 
+                 ;; need to specify this for sablono
+                 ;; when not using devcards
+                 [cljsjs/react "15.3.1-0"]
+                 [cljsjs/react-dom "15.3.1-0"]
+                 #_[org.omcljs/om "1.0.0-alpha46"]
+                 #_[reagent "0.6.0"]
+                 ]
+
+  :plugins [[lein-figwheel "0.5.9"]
+            [lein-cljsbuild "1.1.5" :exclusions [org.clojure/clojure]]]
+
+  :clean-targets ^{:protect false} ["resources/public/js/compiled"
+                                    "target"]
+  
+  :source-paths ["src"]
+
+  :cljsbuild {
+              :builds [{:id "devcards"
+                        :source-paths ["src"]
+                        :figwheel { :devcards true  ;; <- note this
+                                   ;; :open-urls will pop open your application
+                                   ;; in the default browser once Figwheel has
+                                   ;; started and complied your application.
+                                   ;; Comment this out once it no longer serves you.
+                                   :open-urls ["http://localhost:3449/cards.html"]}
+                        :compiler { :main       "squanmate.core"
+                                    :asset-path "js/compiled/devcards_out"
+                                    :output-to  "resources/public/js/compiled/squanmate_devcards.js"
+                                    :output-dir "resources/public/js/compiled/devcards_out"
+                                    :source-map-timestamp true }}
+                       {:id "dev"
+                        :source-paths ["src"]
+                        :figwheel true
+                        :compiler {:main       "squanmate.core"
+                                   :asset-path "js/compiled/out"
+                                   :output-to  "resources/public/js/compiled/squanmate.js"
+                                   :output-dir "resources/public/js/compiled/out"
+                                   :source-map-timestamp true }}
+                       {:id "prod"
+                        :source-paths ["src"]
+                        :compiler {:main       "squanmate.core"
+                                   :asset-path "js/compiled/out"
+                                   :output-to  "resources/public/js/compiled/squanmate.js"
+                                   :optimizations :advanced}}]}
+
+  :figwheel { :css-dirs ["resources/public/css"] }
+
+  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.2"]
+                                  [figwheel-sidecar "0.5.9"]
+                                  [com.cemerick/piggieback "0.2.1"]]
+                   ;; need to add dev source path here to get user.clj loaded
+                   :source-paths ["src" "dev"]
+                   ;; for CIDER
+                   ;; :plugins [[cider/cider-nrepl "0.12.0"]]
+                   :repl-options {; for nREPL dev you really need to limit output
+                                  :init (set! *print-length* 50)
+                                  :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}})
---
+++ src/squanmate/core.cljs
@@ -0,0 +1,24 @@
+(ns squanmate.core
+  (:require
+   #_[om.core :as om :include-macros true]
+   [sablono.core :as sab :include-macros true])
+  (:require-macros
+   [devcards.core :as dc :refer [defcard deftest]]))
+
+(enable-console-print!)
+
+(defcard first-card
+  (sab/html [:div
+             [:h1 "This is your first devcard!"]]))
+
+(defn main []
+  ;; conditionally start the app based on whether the #main-app-area
+  ;; node is on the page
+  (if-let [node (.getElementById js/document "main-app-area")]
+    (.render js/ReactDOM (sab/html [:div "This is working"]) node)))
+
+(main)
+
+;; remember to run lein figwheel and then browse to
+;; http://localhost:3449/cards.html
+
