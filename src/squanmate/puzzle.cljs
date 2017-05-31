7394d5a4aa2742a5c39cf9d56f1e6339367b9c2f
Mika Vilpas <mika.vilpas@gmail.com>
2017-05-31 23:01:37 +0300

    Make piece types more readable in test output

--- src/squanmate/puzzle.cljs
+++ src/squanmate/puzzle.cljs
@@ -1,5 +1,21 @@
 (ns squanmate.puzzle)
 
-(defrecord EdgePiece [])
-(defrecord CornerPiece [])
+(defrecord Piece [type])
 (defrecord Layer [pieces])
+
+(def edge (Piece. "e"))
+(def corner (Piece. "c"))
+
+(def square-layer
+  (let [e edge
+        c corner]
+    (Layer. [c e c e
+             c e c e])))
+
+(defn piece-value [piece]
+  (condp = (:type piece)
+    "e" 2
+    "c" 1))
+
+(defn layer-sliceable? [layer]
+  )
