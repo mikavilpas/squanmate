4583b9b2b99eb725e5c95315f9090e39a5809337
Mika Vilpas <mika.vilpas@gmail.com>
2017-06-10 22:41:02 +0300

    Hide rotation visualizations
    They are simple and not very interesting

--- src/squanmate/ui/alg_visualizer.cljs
+++ src/squanmate/ui/alg_visualizer.cljs
@@ -3,17 +3,29 @@
             [squanmate.alg.execution :as execution]
             [squanmate.ui.drawing.monochrome :as monochrome]))
 
+(defn- interesting-step? [step-either]
+  (either/branch step-either
+                 ;; errors must always be shown
+                 (constantly true)
+                 (fn [s]
+                   (let [interesting #{execution/StartingStepResult
+                                       execution/SliceStepResult}]
+                     (interesting (type s))))))
+
 (defn algorithm-visualization [puzzle alg-string]
   (let [steps (execution/transformations puzzle alg-string)
         key (gensym)]
     [:div
-     (for [[step-either index] (zipmap steps (range))]
-       ^{:key (str key index)}
-       [:div
-        (either/branch step-either
-                       (fn [step]
-                         (println "error encountered: " step)
-                         [:div (pr-str step)])
+     alg-string
+     [:div
+      (for [[step-either index] (zipmap steps (range))
+            :when (interesting-step? step-either)]
+        ^{:key (str key index)}
+        [:div
+         (either/branch step-either
+                        (fn [step]
+                          (println "error encountered: " step)
+                          [:div (pr-str step)])
 
-                       (fn [step]
-                         [monochrome/monochrome-puzzle (:puzzle step)]))])]))
+                        (fn [step]
+                          [monochrome/monochrome-puzzle (:puzzle step)]))])]]))
