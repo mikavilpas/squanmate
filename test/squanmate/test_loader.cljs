64db0f944fa73f9eef1f79a3b95a166f17468bbc
Mika Vilpas <mika.vilpas@gmail.com>
2017-06-11 09:26:57 +0300

    Add file that loads all test files

---
+++ test/squanmate/test_loader.cljs
@@ -0,0 +1,8 @@
+(ns squanmate.test-loader
+  (:require [squanmate.ui.alg-visualizer-test]
+            [squanmate.alg.execution-test]
+            [squanmate.alg.parser-test]
+            [squanmate.puzzle-test]
+            [squanmate.rotation-test]
+            [squanmate.slicing-test]
+            [squanmate.ui.drawing.monochrome-test]))
