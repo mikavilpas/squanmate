0e24569a3d7ecd46cc45e09d286b1739c93592d2
Mika Vilpas <mika.vilpas@gmail.com>
2017-06-11 18:00:56 +0300

    Try to remove poc from core

--- src/squanmate/core.cljs
+++ src/squanmate/core.cljs
@@ -6,13 +6,6 @@
 
 (enable-console-print!)
 
-(def poc-url "http://localhost:9292/cubeshape/cececece?colors=yrb_yb_ybo_yo_yog_yr_ygr_yg")
-
-(defcard-rg render-layer-proof-of-concept
-  [:div [:div
-         "It should be possible to display a layer of the puzzle."]
-   [:div [:img {:src poc-url}]]])
-
 (defn main []
   ;; conditionally start the app based on whether the #main-app-area
   ;; node is on the page
