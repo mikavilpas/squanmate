7d9d0b2462949f62d89d665645447a92ae081070
Mika Vilpas <mika.vilpas@gmail.com>
2017-07-03 10:30:43 +0300

    Add rudimentary support for showing an alg from a starting shape

--- src/squanmate/ui/alg_visualizer.cljs
+++ src/squanmate/ui/alg_visualizer.cljs
@@ -4,7 +4,8 @@
             [squanmate.ui.drawing.monochrome :as monochrome]
             [reagent.core :as reagent]
             [squanmate.ui.shape-chooser :as shape-chooser]
-            [squanmate.puzzle :as puzzle]))
+            [squanmate.puzzle :as puzzle]
+            [cats.monad.either :as either]))
 
 (defn- interesting-step? [step-either]
   (either/branch step-either
@@ -17,26 +18,37 @@
 
 (defn algorithm-visualization [puzzle alg-string]
   (let [steps (execution/transformations puzzle alg-string)]
+    (println steps)
     [:div
-     alg-string
-     [:div
-      (for [[step-either index] (zipmap steps (range))
-            :when (interesting-step? step-either)]
-        ^{:key (str index)}
-        [:div
-         (either/branch step-either
-                        (fn [step]
-                          (println "error encountered: " step)
-                          [:div (pr-str step)])
+     (for [[step-either index] (zipmap steps (range))
+           :when (interesting-step? step-either)]
+       ^{:key (str index)}
+       [:div
+        (either/branch step-either
+                       (fn [step]
+                         (println "error encountered: " step)
+                         [:div (pr-str step)])
 
-                        (fn [step]
-                          [monochrome/monochrome-puzzle (:puzzle step)]))])]]))
+                       (fn [step]
+                         [monochrome/monochrome-puzzle (:puzzle step)]))])]))
 
 (defn alg-visualizer []
-  (let [state (reagent/atom {:puzzle (puzzle/Puzzle. nil nil)})]
+  (let [state (reagent/atom {:puzzle (puzzle/Puzzle. nil nil)
+                             :algorithm ""})]
     (fn render []
       [:div.row
-       [:div.col-xs-8
-        [shape-chooser/puzzle-chooser state]]
-       [:div.col-xs-5
-        [monochrome/monochrome-puzzle (:puzzle @state)]]])))
+       [:form
+        [:div.form-group.col-xs-8
+         [shape-chooser/puzzle-chooser state]]
+
+        [:div.form-group
+         [:input.form-control {:on-change #(swap! state assoc-in [:algorithm]
+                                                  (-> % .-target .-value))
+                               :placeholder "Algorithm"}]]
+        [:div.col-xs-5
+         (let [top (get-in @state [:puzzle :top-layer])
+               bottom (get-in @state [:puzzle :bottom-layer])]
+           (when (and top bottom)
+             [:div
+              [monochrome/monochrome-puzzle (:puzzle @state)]
+              [algorithm-visualization (:puzzle @state) (:algorithm @state)]]))]]])))
