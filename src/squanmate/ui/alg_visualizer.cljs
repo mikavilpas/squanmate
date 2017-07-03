f2d9cb3bfe602239d82d3c1869c9b01bff615714
Mika Vilpas <mika.vilpas@gmail.com>
2017-07-03 12:20:05 +0300

    Allow applying a starting transformation, which will not be drawn as an alg step

--- src/squanmate/ui/alg_visualizer.cljs
+++ src/squanmate/ui/alg_visualizer.cljs
@@ -5,7 +5,8 @@
             [reagent.core :as reagent]
             [squanmate.ui.shape-chooser :as shape-chooser]
             [squanmate.puzzle :as puzzle]
-            [cats.monad.either :as either]))
+            [cats.monad.either :as either]
+            [cats.core :as m]))
 
 (defn- interesting-step? [step-either]
   (either/branch step-either
@@ -16,6 +17,9 @@
                                        execution/SliceStepResult}]
                      (interesting (type s))))))
 
+(defn- show-error [e]
+  [:div (pr-str e)])
+
 (defn algorithm-visualization [puzzle alg-string]
   (let [step-eithers (execution/transformations puzzle alg-string)]
     [:div
@@ -24,14 +28,28 @@
        ^{:key (str index)}
        [:div
         (either/branch step-either
-                       (fn [step]
-                         [:div (pr-str step)])
-
+                       show-error
                        (fn [step]
                          [monochrome/monochrome-puzzle (:puzzle step)]))])]))
 
+(defn- input-box [string-state placeholder]
+  [:input.form-control {:on-change #(reset! string-state (-> % .-target .-value))
+                        :placeholder placeholder}])
+
+(defn- both-layers-present? [puzzle]
+  (and (:top-layer puzzle)
+       (:bottom-layer puzzle)))
+
+(defn- apply-initial-transformation-alg [puzzle alg-string]
+  (let [step-eithers (execution/transformations puzzle alg-string)
+        result-either (last step-eithers)]
+    (either/branch-right result-either
+                         (fn [result]
+                           (either/right (:puzzle result))))))
+
 (defn alg-visualizer []
   (let [state (reagent/atom {:puzzle (puzzle/Puzzle. nil nil)
+                             :initial-rotation ""
                              :algorithm ""})]
     (fn render []
       [:div.row
@@ -40,12 +58,14 @@
          [shape-chooser/puzzle-chooser state]]
 
         [:div.form-group
-         [:input.form-control {:on-change #(swap! state assoc-in [:algorithm]
-                                                  (-> % .-target .-value))
-                               :placeholder "Algorithm"}]]
+         [input-box (reagent/cursor state [:initial-rotation]) "Initial rotation"]
+         [input-box (reagent/cursor state [:algorithm]) "Algorithm"]]
         [:div.col-xs-5
-         (let [top (get-in @state [:puzzle :top-layer])
-               bottom (get-in @state [:puzzle :bottom-layer])]
-           (when (and top bottom)
-             [:div
-              [algorithm-visualization (:puzzle @state) (:algorithm @state)]]))]]])))
+         (when-let [initial-puzzle (and (both-layers-present? (:puzzle @state))
+                                        (apply-initial-transformation-alg (:puzzle @state)
+                                                                          (:initial-rotation @state)))]
+           (either/branch
+            initial-puzzle
+            show-error
+            (fn [initial-puzzle]
+              [algorithm-visualization initial-puzzle (:algorithm @state)])))]]])))
