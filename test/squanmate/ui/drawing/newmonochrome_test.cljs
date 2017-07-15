a237fa0d4ddb7f238ea64abd1a752ef951b3f7ea
Mika Vilpas <mika.vilpas@gmail.com>
2017-07-15 19:48:35 +0300

    Make poc devcard have barrel shield shapes

--- test/squanmate/ui/drawing/newmonochrome_test.cljs
+++ test/squanmate/ui/drawing/newmonochrome_test.cljs
@@ -1,11 +1,13 @@
 (ns squanmate.ui.drawing.newmonochrome-test
   (:require [squanmate.ui.drawing.newmonochrome :as draw]
             [clojure.test :as t]
-            [squanmate.puzzle :as p])
+            [squanmate.puzzle :as p]
+            [squanmate.shapes :as shapes])
   (:require-macros
    [devcards.core :as dc :refer [defcard-rg]]))
 
-(def cube-puzzle p/square-square)
+(def cube-puzzle (p/Puzzle. (p/TopLayer. (:pieces shapes/barrel))
+                            (p/BottomLayer. (:pieces shapes/shield))))
 
 (defcard-rg poc
   [:div
