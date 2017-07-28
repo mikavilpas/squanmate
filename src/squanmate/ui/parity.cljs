9603aa5b4060af2b55489a3f4978b88cd2db4b04
Mika Vilpas <mika.vilpas@gmail.com>
2017-07-28 18:33:41 +0300

    Naive alg parity count component
    Still fails at some cases

--- src/squanmate/ui/parity.cljs
+++ src/squanmate/ui/parity.cljs
@@ -3,7 +3,9 @@
             [squanmate.alg.execution :as execution]
             [squanmate.shapes :as shapes]
             [squanmate.alg.parity-counter :as parity-counter]
-            [cats.monad.either :as either]))
+            [cats.monad.either :as either]
+            [squanmate.puzzle :as puzzle]
+            [clojure.string :as str]))
 
 (defn- square-square? [puzzle]
   (let [layers (shapes/puzzle-layer-shape-names puzzle)
@@ -18,17 +20,30 @@
              "odd"
              "even")]))
 
-(defn alg-parity-switched?-component [transformation-eithers]
-  (let [result-component (m/mlet [start-alg-state (first transformation-eithers)
-                                  finished-alg-state (last transformation-eithers)]
-                                 (let [start (:puzzle start-alg-state)
-                                       finish (:puzzle finished-alg-state)]
-                                   (m/return
-                                    (when (square-square? finish)
-                                      [:div
-                                       [:div "Parity count at start: " [parity-count-component start]]
-                                       [:div "Parity count at finish (todo: this needs to be switched for square square since it counts at the odd position): " [parity-count-component finish]]]))))]
-    (either/branch result-component
-                   (fn [error]
-                     (println "could not determine if the parity has changed due to an error"))
+(defn- alg-parity-switched-at-cubeshape? [alg-string]
+  (let [transformation-eithers (execution/transformations-reverse puzzle/square-square alg-string)
+        starting-step-either (last transformation-eithers)]
+    (m/mlet [start-transformation-step starting-step-either]
+            (m/return [parity-count-component (:puzzle start-transformation-step)]))))
+
+(defn alg-parity-switched-at-cubeshape?-component
+  ;; TODO comments can be moved to the UI in case we need to explain stuff to the user!
+
+  " Note: only call this if you know your `alg-string` is an algorithm that
+  solves some state into cubeshape (square square). Otherwise you will get
+  unreliable results!
+
+  The parity of an algorithm is calculated like this:
+  - start at the solved puzzle (obviously always at even parity)
+  - apply the alg in reverse to get to the starting state
+  - calculate the parity count for this specific state.
+
+  If that count is even, the algorithm goes from even (start) -> even
+  (solved, always even), which means the algorithm preserves parity.
+  Otherwise the algorithm is one that switches the parity once arriving at cubeshape.
+  "
+  [alg-string]
+  (when (not (str/blank? alg-string))
+    (either/branch (alg-parity-switched-at-cubeshape? alg-string)
+                   #(println "could not determine parity of alg: " %)
                    identity)))
