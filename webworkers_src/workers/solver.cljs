ee481cdbf49ae315557a25341e748f504a9dacb5
Mika Vilpas <mika.vilpas@gmail.com>
2017-08-04 15:52:33 +0300

    Fix invalid worker

--- webworkers_src/workers/solver.cljs
+++ webworkers_src/workers/solver.cljs
@@ -5,10 +5,8 @@
 (worker/register :mirror
                  (fn [& arguments]
                    (js/console.log "received arguments: " (str arguments))
-                   (fn [& arguments2]
-                     arguments)))
+                   arguments))
 
 (js/console.log "hello from solver 234!")
-(js/console.log (str @worker/handlers))
 
 (worker/bootstrap)
