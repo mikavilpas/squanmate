e9102b191d12145f5d4f497f74f2bcaf574815d3
Mika Vilpas <mika.vilpas@gmail.com>
2017-08-05 22:40:19 +0300

    Working call

--- src/squanmate/solving.cljs
+++ src/squanmate/solving.cljs
@@ -1,17 +1,25 @@
-(ns squanmate.solving
-  (:require [butler.core :as butler]))
-
-;; there's a different worker file for development / debugging, and production
-(defonce worker-path ;; (if js/goog.DEBUG
-                     ;;   "js/compiled/bootstrap_worker.js"
-                     ;;   "js/compiled/webworkers.js")
-  "js/compiled/webworkers.js")
+(ns squanmate.solving)
 
 (enable-console-print!)
 
-;; Create a web worker with script and handlers.
-(def example-butler (butler/butler worker-path {:foo (fn [res]
-                                                       (println res))}))
+(defn- new-solver []
+  ;; api:
+  ;; new Worker("js/solver-worker.js").proxy()("solve")("start_state_encoded", function(err,result){[]});
+  (js* "new Worker('js/solver-worker.js').proxy()('solve')"))
+
+(defn- solve [starting-state-string]
+  (let [result-atom (atom nil)
+        solver (new-solver)]
+    (solver starting-state-string
+            (fn callback [err, result]
+              (println "received (err,result): " err, result)
+              (when err
+                (reset! result-atom (str "failed: " err)))
+              (when result
+                (reset! result-atom result))))
+    result-atom))
 
 (defn example-call []
-  (butler/work! example-butler :request-foo "foo"))
+  (let [result (solve "A2B3C1D45E6F7G8H")]
+    (js/console.log result)
+    result))
