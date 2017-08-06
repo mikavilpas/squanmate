aa0e3f63a2246512f368c4b7e139e5859f5d706c
Mika Vilpas <mika.vilpas@gmail.com>
2017-08-06 09:45:44 +0300

    Start conversions for solver

--- src/squanmate/solving.cljs
+++ src/squanmate/solving.cljs
@@ -1,4 +1,5 @@
-(ns squanmate.solving)
+(ns squanmate.solving
+  (:require [clojure.string :as str]))
 
 (defn- new-solver []
   ;; api:
@@ -15,3 +16,37 @@
               (when result
                 (reset! result-atom result))))
     result-atom))
+
+(def conversions {[:top :front :left] "A"
+                  [:top :left] "1"
+                  [:top :left :back] "B"
+                  [:top :back] "2"
+                  [:top :back :right] "C"
+                  [:top :right] "3"
+                  [:top :right :front] "D"
+                  [:top :front] "4"
+
+                  [:bottom :front] "5"
+                  [:bottom :front :right] "E"
+                  [:bottom :right] "6"
+                  [:bottom :right :back] "F"
+                  [:bottom :back] "7"
+                  [:bottom :back :left] "G"
+                  [:bottom :left] "8"
+                  [:bottom :left :front] "H"})
+
+(defn- convert-piece [p]
+  (let [piece-id (filterv some?
+                          [(-> p :colors :top)
+                           (-> p :colors :a)
+                           (-> p :colors :b)])]
+    (println p piece-id)
+    (get conversions
+         piece-id
+         (str "oops! piece " (pr-str p) " could not be converted!"))))
+
+(defn convert-to-state-string [puzzle]
+  (let [pieces (into (-> puzzle :top-layer :pieces)
+                     (-> puzzle :bottom-layer :pieces))]
+    (apply str
+           (mapv convert-piece pieces))))
--- test/squanmate/solving_test.cljs
+++ test/squanmate/solving_test.cljs
@@ -2,7 +2,8 @@
   (:require [squanmate.solving :as solving]
             [clojure.test :as t :refer [is async]]
             [cljs.test :as t :include-macros true]
-            [cljs.core.async :refer [<! timeout]])
+            [cljs.core.async :refer [<! timeout]]
+            [squanmate.puzzle :as puzzle])
   (:require-macros
    [devcards.core :as dc :refer [deftest defcard-rg]]
    [cljs.core.async.macros :as m :refer [go]]))
@@ -15,3 +16,8 @@
              (is (= "( 0, 2)/(-5, 4)/( 5, 2)/(-3, 0)/( 0, 3)/(-5, 1)/(-1, 2)/(-3,-2)"
                     @result-atom))
              (done)))))
+
+(deftest convert-to-state-string-test []
+  (is (= "A1B2C3D45E6F7G8H"
+         (solving/convert-to-state-string puzzle/square-square))
+      "should be able to convert square square to solver notation"))
