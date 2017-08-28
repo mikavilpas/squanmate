52fa823b6f1eaff92d8378a3b05f6bb6939c559d
Mika Vilpas <mika.vilpas@gmail.com>
2017-08-28 20:11:39 +0300

    Update initial-rotation-adjuster-test to new rotation formats

--- test/squanmate/ui/initial_rotation_adjuster_test.cljs
+++ test/squanmate/ui/initial_rotation_adjuster_test.cljs
@@ -6,12 +6,12 @@
    [devcards.core :as dc :refer [defcard-rg deftest]]))
 
 (deftest possible-sliceable-rotations-test []
-  (is (= {:positive [3]
-          :negative [-3]}
+  (is (= {:positive [6]
+          :negative []}
          (ira/possible-sliceable-rotations shapes/parallel-edges))
       "returns only rotations that are sliceable")
 
   (is (= {:positive [1 3 4 6]
-          :negative [-2 -3 -5 -6]}
+          :negative [-2 -3 -5]}
          (ira/possible-sliceable-rotations shapes/square))
       "returns shorter rotations first"))
