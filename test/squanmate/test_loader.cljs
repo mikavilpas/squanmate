20c35479101330b7dd4f81011b529b2b1b7b871c
Mika Vilpas <mika.vilpas@gmail.com>
2017-09-15 19:04:02 +0300

    Add Window.p (cljs.pprint.pprint) shortcut for debugging

--- test/squanmate/test_loader.cljs
+++ test/squanmate/test_loader.cljs
@@ -19,3 +19,7 @@
             squanmate.ui.color-chooser-test
             squanmate.services.count-positions-test
             squanmate.ui.count-position-finder-test))
+
+;; use this when at a breakpoint in the browser. It prints human readable and
+;; pretty presentations of clojurescript things.
+(js* "var p = cljs.pprint.pprint")
