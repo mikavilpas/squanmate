bbb2b4f8f2fff545e5cc6074370426c4feeb0747
Mika Vilpas <mika.vilpas@gmail.com>
2017-09-16 08:30:27 +0300

    Add a couple of test cases for count-positions-test

--- test/squanmate/services/count_positions_test.cljs
+++ test/squanmate/services/count_positions_test.cljs
@@ -3,10 +3,21 @@
             [squanmate.services.count-positions :as count-positions]
             [cljs.test :as t :refer [is]]
             [cats.monad.either :as either]
-            [squanmate.shapes :as shapes])
+            [squanmate.shapes :as shapes]
+            [squanmate.ui.drawing.newmonochrome :as newmonochrome])
   (:require-macros
    [devcards.core :as dc :refer [defcard-rg deftest]]))
 
 (deftest count-positions-test []
   (is (= #{#{0 3 -3 6} #{1 -2 4 -5}}
-         (count-positions/count-positions shapes/square))))
+         (count-positions/count-positions shapes/square)))
+
+  (is (= #{#{0 3 -3 6} #{2 -2 4 -4}}
+         (count-positions/count-positions shapes/barrel)))
+
+  (is (= #{#{0 6}
+           #{1 -5}}
+         (count-positions/count-positions shapes/mushroom))))
+
+(defcard-rg mushroom
+  [newmonochrome/layer-component shapes/mushroom])
