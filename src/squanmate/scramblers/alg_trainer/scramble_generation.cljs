cd3f9b1b1cd9ed1f323ce62f5175b1f78d4bd7a0
Mika Vilpas <mika.vilpas@gmail.com>
2017-10-13 19:00:02 +0300

    Create scramble for U2 M2 U2 M2 case
    It shows up all weird, which is a good thing. Now it's very difficult to try to
    deduce the (optimized) alg that solves this case.

--- src/squanmate/scramblers/alg_trainer/scramble_generation.cljs
+++ src/squanmate/scramblers/alg_trainer/scramble_generation.cljs
@@ -1,11 +1,11 @@
 (ns squanmate.scramblers.alg-trainer.scramble-generation
-  (:require [squanmate.puzzle :as p]
-            [cats.core :as m]
+  (:require [cats.monad.either :as either]
             [squanmate.alg.execution :as execution]
-            [squanmate.solving :as solving]
-            [squanmate.rotation :as rotation]))
+            [squanmate.puzzle :as p]
+            [squanmate.rotation :as rotation]
+            [squanmate.solving :as solving]))
 
-(defn- apply-random-rotation-of-3 [puzzle]
+(defn- apply-starting-rotation [puzzle]
   (letfn [(rotate [layer]
             (let [[new-layer _amount] (rand-nth (rotation/possible-rotations
                                                  layer
@@ -15,6 +15,22 @@
             :top-layer rotate
             :bottom-layer rotate)))
 
+(defn- report-error-for-case [alg e]
+  (js/console.log "Error setting up case for alg: " alg ". Description: " e))
+
+(defn- set-scramble-for-start-position! [state end-step-result]
+  (let [puzzle (:puzzle end-step-result)]
+    (solving/solve-and-generate-scramble puzzle state)))
+
 (defn new-scramble [state]
-  (let [start (apply-random-rotation-of-3 p/square-square)]
-    (solving/solve-and-generate-scramble start state)))
+  ;; a starting rotation makes it possible to get the same EP case in all
+  ;; different orientations
+  (let [start (apply-starting-rotation p/square-square)
+
+        ;; todo get correct alg
+        alg "U2 M2 U2 M2"
+
+        alg-starting-state (execution/transformation-result-reverse start alg)]
+    (either/branch alg-starting-state
+                   #(report-error-for-case alg %)
+                   #(set-scramble-for-start-position! state %))))
