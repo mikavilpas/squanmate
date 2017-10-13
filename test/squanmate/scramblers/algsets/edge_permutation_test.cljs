a24432f32b48795d3775e8112ae0e2bc2ccec3e4
Mika Vilpas <mika.vilpas@gmail.com>
2017-10-13 10:06:43 +0300

    Start correcting a lot of EP cases

--- test/squanmate/scramblers/algsets/edge_permutation_test.cljs
+++ test/squanmate/scramblers/algsets/edge_permutation_test.cljs
@@ -4,7 +4,8 @@
             [squanmate.puzzle :as p]
             [squanmate.alg.parser :as parser]
             [cats.monad.either :as either]
-            [squanmate.ui.common :as common])
+            [squanmate.ui.common :as common]
+            [squanmate.alg.execution :as execution])
   (:require-macros [devcards.core :as dc :refer [defcard-rg deftest]]))
 
 (defn parse [[case-name alg]]
@@ -18,3 +19,17 @@
 
 (deftest parse-all-ep-cases-test []
   (is (empty? (non-parseable-cases ep/all-cases-unordered))))
+
+(defn execute-reverse [[case-name alg]]
+  [case-name
+   (execution/transformation-result-reverse p/square-square
+                                            alg)])
+
+(defn non-executable-cases [cases]
+  (let [execution-results (map execute-reverse cases)]
+    (->> execution-results
+         (filter (fn [[case-name result]]
+                   (either/left? result))))))
+
+(deftest execute-all-ep-cases-test []
+  (is (empty? (non-executable-cases ep/all-cases-unordered))))
