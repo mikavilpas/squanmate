aba9dac22a3cd3835fb1bb4763b4ac88fbc55046
Mika Vilpas <mika.vilpas@gmail.com>
2017-11-09 10:40:17 +0200

    Mention the possibility of using keys to answer in the ui

--- src/squanmate/pages/parity_game.cljs
+++ src/squanmate/pages/parity_game.cljs
@@ -2,7 +2,8 @@
   (:require [squanmate.ui.parity-sequences :as ps]
             [reagent.core :as reagent]
             [squanmate.pages.page-content :as page-content]
-            [squanmate.services.keyboard :as keyboard]))
+            [squanmate.services.keyboard :as keyboard]
+            [squanmate.ui.common :as common]))
 
 (defonce state (ps/default-state))
 
@@ -14,6 +15,8 @@
      [:p "This is a trainer to help you recognize the parity of color sequences.
      This is a very useful skill in the Cale Schoon parity method, one that you
      will have to master if you intend to use it."]
+     [common/help-block [common/glyphicon {:glyph :info-sign}]
+      " You can use the left and right keys to answer too"]
      [ps/parity-game state]]]])
 
 (defmethod page-content/page :parity-game []
