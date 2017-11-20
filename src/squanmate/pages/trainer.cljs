486cd907e34d6fcaaf7f8d8a6d1a1d59ff5a2516
Mika Vilpas <mika.vilpas@gmail.com>
2017-11-20 10:41:40 +0200

    Save settings whenever they are changed in trainer

--- src/squanmate/pages/trainer.cljs
+++ src/squanmate/pages/trainer.cljs
@@ -1,8 +1,20 @@
 (ns squanmate.pages.trainer
   (:require [squanmate.scramblers.shape-scrambler :as shape-scrambler]
-            [squanmate.pages.page-content :as page-content]))
+            [squanmate.pages.page-content :as page-content]
+            [squanmate.services.storage :as storage]
+            [squanmate.scramblers.shape-scrambler.actions :as a]))
 
-(defonce page-state (shape-scrambler/new-state))
+(defn initial-state []
+  (let [settings-atom (shape-scrambler/new-state)
+        maybe-saved-settings (a/try-load-settings)]
+    (when maybe-saved-settings
+      (swap! settings-atom merge maybe-saved-settings))
+    (add-watch settings-atom nil
+               (fn [_key _ref _old-value new-state]
+                 (a/save-settings! new-state)))
+    settings-atom))
+
+(defonce page-state (initial-state))
 
 (defn content []
   [:div
