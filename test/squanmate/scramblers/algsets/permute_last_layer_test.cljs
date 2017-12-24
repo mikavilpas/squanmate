06fc1f8315f2382312a61087beb5cebc8233374b
Mika Vilpas <mika.vilpas@gmail.com>
2017-12-24 08:42:00 +0200

    Divide pll algs into odd and even parity-wise

--- test/squanmate/scramblers/algsets/permute_last_layer_test.cljs
+++ test/squanmate/scramblers/algsets/permute_last_layer_test.cljs
@@ -1,7 +1,9 @@
 (ns squanmate.scramblers.algsets.permute-last-layer-test
   (:require [clojure.test :as t :refer [is]]
             [squanmate.scramblers.algsets.permute-last-layer :as pll]
-            [squanmate.utils.alg-verification-utils :as av])
+            [squanmate.utils.alg-verification-utils :as av]
+            [cats.monad.either :as either]
+            [squanmate.scramblers.algsets.algset :as algset])
   (:require-macros [devcards.core :as dc :refer [deftest]]))
 
 (deftest parse-all-cases-test []
@@ -12,3 +14,12 @@
 
 (deftest all-cases-are-aligned-test []
   (is (empty? (av/non-aligned-cases pll/all-cases))))
+
+(deftest parity-of-cases []
+  (let [results (av/parity-and-non-parity-cases pll/all-cases)
+        odd-cases (get results :switches-parity)
+        even-cases (get results :preserves-parity)]
+    (is (= (:even-cases pll/pll-algset)
+           even-cases))
+    (is (= (:odd-cases pll/pll-algset)
+           odd-cases))))
