418b9d91e04425fa79e6006ea8a7ef45d239270b
Mika Vilpas <mika.vilpas@gmail.com>
2017-12-25 18:56:24 +0200

    tnloiatnloiatn

--- test_runner/squanmate/test_runner.cljs
+++ test_runner/squanmate/test_runner.cljs
@@ -12,4 +12,11 @@
 ;; needs to be executed when this file is evaluated. This is required by
 (runner/set-entry-point!
  (fn []
-   (test/run-all-tests #"^(?!(squanmate.solving-test)$).*$")))
+   ;; ignore squanmate.solving-test for now. It has dependencies to
+   ;; web-worker-proxies that are not included in the js build (they are loaded
+   ;; externally in the browser). This causes them to rightfully not exist when
+   ;; testing.
+   ;;
+   ;; also: this test api is based on macros, which means the argument must be a
+   ;; literal string.
+   (test/run-all-tests #"^squanmate.(?!(solving-test)$).*$")))
