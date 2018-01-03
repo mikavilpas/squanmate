de2049eb78d5d3e60edbce4031ae6be902099a9e
Mika Vilpas <mika.vilpas@gmail.com>
2018-01-03 12:23:33 +0200

    Add global colors atom

--- src/squanmate/services/global_colors_store.cljs
+++ src/squanmate/services/global_colors_store.cljs
@@ -1,13 +1,20 @@
 (ns squanmate.services.global-colors-store
-  "Operates on newmonochrome/default-settings like maps"
+  "Operates on newmonochrome/default-settings - like maps"
   (:require [squanmate.services.storage :as storage]
-            [squanmate.ui.drawing.newmonochrome :as newmonochrome]))
+            [squanmate.ui.drawing.newmonochrome :as newmonochrome]
+            [reagent.core :as reagent]))
 
 (def ^:private setting-name "global-colors")
 
-(defn save! [color-state]
-  (storage/save setting-name color-state))
-
 (defn get-or-default! []
+  (js/console.log "loading color settings: " (storage/get-value setting-name))
   (or (storage/get-value setting-name)
       newmonochrome/default-settings))
+
+(defonce settings-atom
+  (reagent/atom (get-or-default!)))
+
+(defn save! [color-state]
+  (js/console.log "saving global colors: " color-state)
+  (storage/save setting-name color-state)
+  (reset! settings-atom color-state))
