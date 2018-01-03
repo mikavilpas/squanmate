338364c740eaaa0d498cf1657ca2abb0c5984fe1
Mika Vilpas <mika.vilpas@gmail.com>
2018-01-03 09:43:57 +0200

    Make scramble inspector use global colors

--- src/squanmate/pages/scramble_inspector.cljs
+++ src/squanmate/pages/scramble_inspector.cljs
@@ -1,15 +1,22 @@
 (ns squanmate.pages.scramble-inspector
   (:require [squanmate.ui.scramble :as scramble]
             [squanmate.alg.manipulation :as manipulation]
-            [squanmate.pages.page-content :as page-content]))
+            [squanmate.pages.page-content :as page-content]
+            [squanmate.services.global-colors-store :as global-colors-store]))
 
-(defonce state (scramble/default-state))
+(defn- new-state []
+  (let [state (scramble/default-state)
+        global-colors (global-colors-store/get-or-default!)]
+    (swap! state merge {:draw-settings global-colors})
+    state))
+
+(defonce state (new-state))
 
 (defn content []
   [scramble/component state])
 
 (defn content-from-args [{:keys [scramble-algorithm]}]
-  (let [local-state (scramble/default-state)
+  (let [local-state (new-state)
         scramble-algorithm (manipulation/format-alg scramble-algorithm)]
     (swap! local-state assoc-in [:scramble :scramble-algorithm] scramble-algorithm)
     (scramble/mark-alg-imported local-state)
