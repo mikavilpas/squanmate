3b470e619ed0f5ba9d30dc5d5077825ae2841525
Mika Vilpas <mika.vilpas@gmail.com>
2018-01-03 12:40:32 +0200

    Remove extra settings in tests

--- test/squanmate/ui/alg_visualizer_test.cljs
+++ test/squanmate/ui/alg_visualizer_test.cljs
@@ -21,14 +21,16 @@
 (defcard-rg turn-and-slice
   [v/algorithm-visualization
    puzzle/square-square
-   "3/"])
+   "3/"
+   {:monochrome? false}])
 
 (defcard-rg square-square-alg
   [:div
    "this is the lin method algorithm for the CP+DF opposite corners case"
    [v/algorithm-visualization
     puzzle/square-square
-    "1,0 / -4,-3 / -3,0 / -3,-3 / -3,0 / -2,-3 /"]])
+    "1,0 / -4,-3 / -3,0 / -3,-3 / -3,0 / -2,-3 /"
+    {:monochrome? false}]])
 
 (defonce alg-visualizer-state (v/default-alg-visualizer-state))
 
