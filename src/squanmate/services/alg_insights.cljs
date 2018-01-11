d796a3cbf12b4bcde935a652dd838bbc4aa9adf0
Mika Vilpas <mika.vilpas@gmail.com>
2018-01-11 10:07:14 +0200

    Allow combining markers

--- src/squanmate/services/alg_insights.cljs
+++ src/squanmate/services/alg_insights.cljs
@@ -9,20 +9,30 @@
             [squanmate.puzzle :as p]
             [squanmate.services.alg-insights.cubeshape :as cubeshape]))
 
-(defn- alg-with-markers
-  "Combines the parsed parts of the scramble algorithm with markers that will be
-  attached to each specific algorithm token."
-  [display-tokens markers]
-  )
+(defrecord Token [move markers])
 
-(defn- executio-steps [alg-string]
+(defn- combine-alg-with-markers
+  "Combines the parsed parts of the scramble algorithm with markers that will be
+  attached to each specific algorithm token.
+
+  `markers-maps` should be a sequence of maps in the insights format. It must be
+  as long as `display-tokens`.
+  "
+  [display-tokens markers-maps]
+  (for [[index token] (zipmap (range) display-tokens)]
+    (let [markers (->> markers-maps
+                       (map #(get % index))
+                       vec)]
+      (->Token token markers))))
+
+(defn- execution-steps [alg-string]
   (let [alg-steps (execution/transformations p/square-square alg-string)
         steps-either (eu/list-of-eithers->either-list alg-steps)]
     steps-either))
 
 (defn alg-with-cubeshape-status-highlighted [alg-string]
   (m/mlet [display-tokens (parser/parse alg-string)
-           execution-steps (scramble-steps alg-string)]
+           steps (execution-steps alg-string)]
           (m/return
-           (let [markers (cubeshape/entered-and-left-cubeshape execution-steps)]
-             (alg-with-markers display-tokens markers)))))
+           (let [markers (cubeshape/entered-and-left-cubeshape steps)]
+             (combine-alg-with-markers display-tokens [markers])))))
--- test/squanmate/services/alg_insights_test.cljs
+++ test/squanmate/services/alg_insights_test.cljs
@@ -1,7 +1,22 @@
 (ns squanmate.services.alg-insights-test
   (:require [cats.core :as m]
             [cljs.test :as t :refer [is]]
-            [squanmate.alg.execution :as execution]
-            [squanmate.puzzle :as p]
+            [squanmate.alg.types :as types]
             [squanmate.services.alg-insights :as sut])
   (:require-macros [devcards.core :as dc :refer [deftest]]))
+
+(def slice (types/->Slice))
+(defn rotations [top bottom]
+  (types/->Rotations top bottom))
+
+(def token sut/map->Token)
+
+(deftest alg-with-cubeshape-status-highlighted-test []
+  (is (= [(token {:move slice, :markers [:in-cubeshape]})
+          (token {:move (rotations 6 0), :markers [:shape-shifted]})
+          (token {:move slice, :markers [:shape-shifted]})
+          (token {:move (rotations 6 0), :markers [:shape-shifted]})
+          (token {:move slice, :markers [:shape-shifted]})]
+         (m/extract
+          (sut/alg-with-cubeshape-status-highlighted "/6/6/")))
+      "returns a sequence of algorithm moves and their associated insight markers"))
