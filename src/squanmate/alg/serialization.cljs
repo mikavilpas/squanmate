349ad8538ea4a4ba12e4abca5e6b083cc65c23d9
Mika Vilpas <mika.vilpas@gmail.com>
2018-01-19 11:08:33 +0200

    Remove outdated code comment

--- src/squanmate/alg/serialization.cljs
+++ src/squanmate/alg/serialization.cljs
@@ -7,9 +7,6 @@
 
 (defn step-to-str [s]
   (condp = (type s)
-    ;; carefully placing spaces only outside of (a,-b)/ groups
-    ;; makes the scramble to be rendered with very nice text
-    ;; wrapping in the browser :)
     types/Slice "/"
     types/Rotations (str "(" (prettification/prettify-value
                               (:top-amount s))
