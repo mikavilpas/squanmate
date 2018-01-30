c82343cdc7a97853e91d032f3ae01c8e10476587
Mika Vilpas <mika.vilpas@gmail.com>
2018-01-30 13:17:48 +0200

    Add keyboard bindings for cubeshape trainer

--- src/squanmate/pages/trainer.cljs
+++ src/squanmate/pages/trainer.cljs
@@ -3,7 +3,8 @@
             [squanmate.scramblers.shape-scrambler :as shape-scrambler]
             [squanmate.scramblers.shape-scrambler.actions :as a]
             [squanmate.scramblers.shape-scrambler.settings-handler :as settings]
-            [squanmate.services.global-colors-store :as global-colors-store]))
+            [squanmate.services.global-colors-store :as global-colors-store]
+            [squanmate.services.keyboard :as keyboard]))
 
 (defn initial-state []
   (let [settings-atom (shape-scrambler/new-state)
@@ -22,10 +23,21 @@
 
 (defonce page-state (initial-state))
 
-(defn content []
+(defn content [state]
   [:div
    [shape-scrambler/scramble-component page-state @global-colors-store/settings-atom]])
 
+(defn- set-keyboard-bindings! [state]
+  (keyboard/bind! "space" (keyboard/prevent-bubbling
+                           #(a/set-new-random-scramble state)))
+  (keyboard/bind! "shift+space" (keyboard/prevent-bubbling
+                                 #(a/deselect-case-and-generate-new-scramble! state)))
+
+  (keyboard/bind! "r r" #(a/set-new-repeat-scramble state))
+  (keyboard/bind! "r s" #(a/set-new-scramble-with-parity state :same-relative-parity))
+  (keyboard/bind! "r o" #(a/set-new-scramble-with-parity state :opposite-relative-parity)))
+
 (defmethod page-content/page :trainer []
+  (set-keyboard-bindings! page-state)
   [:div
-   [content]])
+   [content page-state]])
