f2a6f54cddf135fda60079efa8ac36d316546d85
Mika Vilpas <mika.vilpas@gmail.com>
2018-01-31 12:28:53 +0200

    Add space -> new scramble shortcut to alg trainer

--- src/squanmate/services/keyboard.cljs
+++ src/squanmate/services/keyboard.cljs
@@ -3,14 +3,14 @@
 
 ;; documentation: https://craig.is/killing/mice
 
-(defn prevent-bubbling [&]
+(def prevent-bubbling
   ;; Returning false will cause mousetrap to stop the browser's default action
   ;; defined for the current key binding.
   ;;
   ;; This can be used to bind the space or enter keys to a button. Normally the
   ;; keyboard event would bubble up, which would cause the keybinding to be
   ;; triggered in addition to the button being clicked (via space or enter)
-  false)
+  (constantly false))
 
 (defn bind! [key-or-combo callback]
   (js/Mousetrap.bind key-or-combo callback))
